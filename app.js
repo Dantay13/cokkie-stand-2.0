47c48207d0764b8a8aef425f5c9c566eb6cd60b7
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2022-11-03 23:32:39 -0500

    created the different locations as objects with properties in my js document

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 'strict use'
+
+let seattle = {
+  min: 23,
+  max: 65,
+  avgCookies: 6.3
+}
+
+let tokyo = {
+    min: 3,
+    max: 24,
+    avgCookies: 1.2
+}
+
+let dubai = {
+    min: 11,
+    max: 38,
+    avgCookies: 3.7
+}
+
+let paris = {
+    min: 20,   
+    max: 38,
+    avgCookies: 2.3
+}
+
+let lima = {
+    min: 2,
+    max: 16,
+    avgCookies: 4.6
+}
+
+console.log(seattle, tokyo, dubai, paris, lima);
