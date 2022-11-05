35558eba4306ad7900981273f9d097fcc60f3142
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2022-11-05 03:40:30 -0500

    added gitignore file to my document, fixed the random number generator function and make it so that it return an integer

--- app.js
+++ app.js
@@ -5,39 +5,72 @@ let seattle = {
   max: 65,
   avgCookies: 6.3,
   calRandomNum: function randomNumber(min, max) {
-      return Math.random() * (max - min) + min;
-    },
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
 
-seattle.calRandomNum();
+seattle.calRandomNum(23,65);
 
-console.log('this is the random numbers:', seattle.calRandomNum);
+console.log('seattle random num:',seattle.calRandomNum(23,65));
 
 let tokyo = {
     min: 3,
     max: 24,
     avgCookies: 1.2,
     calRandomNum: function randomNumber(min, max) {
-        return Math.random() * (max - min) + min;
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
       }
 }
+
+tokyo.calRandomNum(3,24);
+
+console.log('tokyo random num:',tokyo.calRandomNum(3,24));
 
 let dubai = {
     min: 11,
     max: 38,
-    avgCookies: 3.7
+    avgCookies: 3.7,
+    calRandomNum: function randomNumber(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
 }
+
+dubai.calRandomNum(11,38);
+
+console.log('dubai random num:', dubai.calRandomNum(11,38));
 
 let paris = {
     min: 20,   
     max: 38,
-    avgCookies: 2.3
+    avgCookies: 2.3,
+    calRandomNum: function randomNumber(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max -min + 1) + min);
+    }
 }
+
+paris.calRandomNum(20, 38);
+console.log('paris random num:', paris.calRandomNum(20, 38));
 
 let lima = {
     min: 2,
     max: 16,
-    avgCookies: 4.6
+    avgCookies: 4.6,
+    calRandomNum: function randomNumber(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 }
 
-console.log(seattle, tokyo, dubai, paris, lima);
+lima.calRandomNum(2, 16);
+console.log('lima random num:', lima.calRandomNum(2, 16));
+
+// console.log(seattle, tokyo, dubai, paris, lima);
